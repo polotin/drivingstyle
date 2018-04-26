7d3b85114bf4b56b4d9a7ddbb32047180a47d628
polotin <18201912117@163.com>
2018-04-26 19:20:10 +0800

    l

--- pages/lane_change_detection.php
+++ pages/lane_change_detection.php
@@ -8,12 +8,14 @@
 
 function lane_change_detection($video_path,$csv_path, $with_video_flag){
 //    $result = exec('python lane_changing_detection.py {$video_path} {$csv_path}', $out, $res);
-    $result = shell_exec('python lane_changing_detection.py '.$video_path.' '.$csv_path.' '.$with_video_flag);
-//    global $laneChangeEvent;
+    $cmd = 'python lane_changing_detection.py '.$video_path.' '.$csv_path.' '.$with_video_flag;
+    $result = shell_exec($cmd);
+    global $laneChangeEvent;
+    $laneChangeEvent += $result;
 //    if($result!= null && $result!='None'){
 //        $laneChangeEvent += $result.',';
 //    }else{
 //        $laneChangeEvent += "None";
 //    }
-    echo "<script type=text/javascript>console.log('" . "result:" . $result . "')</script>";
+    echo "<script type=text/javascript>console.log('" . "result:" . $laneChangeEvent . "')</script>";
 }
