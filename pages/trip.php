75114cb88042017097659cc0ca3a33cef41a9fae
polotin <18201912117@163.com>
2018-04-29 15:27:20 +0800

    l

---
+++ pages/trip.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/4/29
+ * Time: 14:51
+ */
+
+class trip
+{
+    public $driver_id="";
+    public $trip_id="";
+    public $stop_count = 0;
+    public $go_count = 0;
+    public $hb_count = 0;
+    public $turn_count = 0;
+    public $hs_count = 0;
+    public $lc_count = 0;
+    public $cf_count = 0;
+}
