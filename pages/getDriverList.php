520be8422ed1ef95e59a7e57d47876deeb3ed201
polotin <18201912117@163.com>
2018-06-05 12:45:30 +0800

    l

---
+++ pages/getDriverList.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/5/1
+ * Time: 16:45
+ */
+$csv_dir = "";
+if (isset($_GET["csv_dir"])) {
+    $csv_dir = $_GET["csv_dir"];
+} else echo "";
+if(is_dir($csv_dir)){
+    getDriverList();
+}else{
+    echo "";
+    return;
+}
+
+
+$file_names = array();
+function getDriverList(){
+    global $csv_dir;
+    $files_folder = scandir($csv_dir);
+    foreach ($files_folder as $name) {
+        if (startWith("CCHN", $name) && endWith(".csv", $name)) {
+            $file_names[] = $name;
+        }
+    }
+    if (!empty($file_names)) {
+        echo implode(',',$file_names);
+    }
+}
+function startWith($needle, $name)
+{
+    return strpos($name, $needle) === 0;
+}
+function endWith($needle, $name)
+{
+    $length = strlen($needle);
+    if ($length == 0) {
+        return true;
+    }
+    return (substr($name, -$length) === $needle);
+}
